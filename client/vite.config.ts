c493fd4e92eee98f0145382771ea60df5485226d
Andrew Sunada <kaleb2azriel@gmail.com>
2025-11-28 14:28:08 -0800

    setting vite api url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(
+      process.env.VITE_API_URL || 'https://bingo--wemove-ba867.us-east4.hosted.app/api'
+    ),
+  },
+});
